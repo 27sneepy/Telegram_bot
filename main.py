4cbb0105c90bfda3a7574f67e1f1027f0d635eda
unitf40gmail.com <unitf40@gmail.com>
2026-03-14 19:50:38 +0200

    /

--- main.py
+++ main.py
@@ -1,47 +1,63 @@
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 from asyncio import run
 from config import BOT_TOKEN
 import os
+from aiogram.filters import Command
+import asyncio
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
-
-    # @dp.message(F.photo)
-    # async def get_photo(message: Message, bot: Bot):
-    #     print(f"[LOG] пользователь {message.from_user.id} вызвал функцию get_photo")
-    #     photo=message.photo[-1]
-    #     file = await bot.get_file(photo.file_id)
-    #     print(f"[LOG] получение файла {file.file_name}")
-    #     PATH=os.path.join("files",f"{file.file_unique_id}.jpg")
-    #     print(PATH)
-    #     await bot.download_file(file.file_path,destination=PATH)
-    #     print(f"[LOG] сохранение файла {PATH}")
-    #     print(file)
-    #     await message.answer("cool photo")
-    @dp.message(F.photo | F.video)
-    async def get_photo_video(message: Message, bot: Bot):
-        print(f"[LOG] Пользователь {message.from_user.id} вызвал функцию get_photo_video")
-        os.makedirs("downloads", exist_ok=True)
-        if not message.photo:
-            file = await bot.get_file(message.video.file_id)
-            print(f'[LOG] Файл {file.file_unique_id} получен')
-            PATH = os.path.join("downloads", f"{file.file_unique_id}.jpg")
-
+    # @dp.message(F.photo | F.video | F.voice)
+    # async def get_photo_video_voice(message: Message, bot: Bot):
+    #     print(f"[LOG] Пользователь {message.from_user.id} вызвал функцию get_photo_video_voice")
+    #     os.makedirs("downloads", exist_ok=True)
+    #     if message.photo:
+    #         file = await bot.get_file(message.photo[-1].file_id)
+    #         print(f'[LOG] Файл {file.file_unique_id} получен')
+    #         PATH = os.path.join("downloads", f"{file.file_unique_id}.jpg")
+    #     elif message.voice:
+    #         file = await bot.get_file(message.voice.file_id)
+    #         PATH = os.path.join("downloads", f"{file.file_unique_id}.ogg")
+    #     else:
+    #         file = await bot.get_file(message.video.file_id)
+    #         print(f'[LOG] Файл {file.file_unique_id} получен')
+    #         PATH = os.path.join("downloads", f"{file.file_unique_id}.mp4")
+    #     print(f"[LOG] начало скачивания {file}, по пути {PATH}")
+    #     await bot.download_file(file.file_path, destination=PATH)
+    #     print(f'[LOG] Файл {PATH} сохранен в соответствующую директорию')
+    #
+    #     await message.answer("крутое фото или видео или гс")
+    # @dp.message(F.sticker)
+    # async def get_sticker(message: Message):
+    #     print(f"[LOG] пользователь {message.from_user.id} вызвал функцию get_sticker")
+    #     with open("stickers.txt", "a+") as f:
+    #         f.write(message.sticker.file_id + "\n")
+    #         print(f"[LOG] записан стикер {message.sticker.file_id}")
+    # @dp.message(F.text == "отправь фото")
+    # async def send_photo(message: Message):
+    #     print(f"[LOG] получен запрос от {message.from_user.id} в send_photo")
+    #     PATH = os.path.join("downloads","123123.jpg")
+    #     print(f"[LOG] начало бинаризации")
+    #     photo=FSInputFile(PATH)
+    #     print(f"[LOG] конец бинаризации")
+    #     await message.answer_photo(photo=photo,caption="это география")
+    # #     await message.answer_photo("https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/11582/production/_103424017_mary-mcgowan_caught-in-the-act_00001294.jpg.webp",caption="это белка")
+    @dp.message(Command(commands=["show"]))
+    async def digits_handler(message: Message):
+        if os.path.exists("data.txt"):
+            with open("data.txt", "r") as f:
+                list_data=f.readlines()
+                for i in list_data[1:]:
+                    elements=i.split()
+                    await message.answer(f"Текущая температура на улице: {elements[1]}")
+                    await asyncio.sleep(10)
         else:
-            file = await bot.get_file(message.photo[-1].file_id)
-            print(f'[LOG] Файл {file.file_unique_id} получен')
-            PATH = os.path.join("downloads", f"{file.file_unique_id}.mp4")
+            await message.answer("файла нету((")
 
-        await bot.download_file(file.file_path, destination=PATH)
-        print(f'[LOG] Файл {PATH} сохранен в соответствующую директорию')
-
-        await message.answer("крутое фото или видео")
 
     await dp.start_polling(bot)
-
 print(f'[LOG] Бот запущен')
-
 if __name__ == '__main__':
     run(main()) # запускает цикла событий(dispatcher)
