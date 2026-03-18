a3cc9015f48894685aceea2e270c31a704d005e7
unitf40gmail.com <unitf40@gmail.com>
2026-03-18 20:15:12 +0200

    /

--- main.py
+++ main.py
@@ -45,27 +45,52 @@ async def main():
     #     print(f"[LOG] конец бинаризации")
     #     await message.answer_photo(photo=photo,caption="это география")
     # #     await message.answer_photo("https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/11582/production/_103424017_mary-mcgowan_caught-in-the-act_00001294.jpg.webp",caption="это белка")
-    @dp.message(Command(commands=["show"]))
-    async def digits_handler(message: Message):
-        file_name="data.txt"
-        if not os.path.exists(file_name):
-            with open(file_name, "w") as f:
-                f.write("curs temperature\n")
-                for _ in range(3):
-                    f.write(f"{random.randint(0, 100)} {random.randint(0, 100)}\n")
-        with open(file_name, "r") as f:
-            list_data = f.readlines()
-        if len(list_data) <= 1:
-            await message.answer("Нет данных в файле")
-            return
-        msg = await message.answer("Текущая температура на улице: __")
-        for line in list_data[1:]:
-            elements = line.split()
-            await msg.edit_text(f"Текущая температура на улице: {elements[1]}")
-            await asyncio.sleep(5)
-        await asyncio.sleep(3)
-        await msg.delete()
-
+    # @dp.message(Command(commands=["show"]))
+    # async def digits_handler(message: Message):
+    #     file_name="data.txt"
+    #     if not os.path.exists(file_name):
+    #         with open(file_name, "w") as f:
+    #             f.write("curs temperature\n")
+    #             for _ in range(3):
+    #                 f.write(f"{random.randint(0, 100)} {random.randint(0, 100)}\n")
+    #     with open(file_name, "r") as f:
+    #         list_data = f.readlines()
+    #         if len(list_data) <= 1:
+    #             await message.answer("Нет данных в файле")
+    #             return
+    #         msg = await message.answer("Загрузка...")
+    #         for line in list_data[1:]:
+    #             elements = line.split()
+    #             await msg.edit_text(f"Текущая температура на улице: {elements[1]}")
+    #             await asyncio.sleep(5)
+    #         await msg.delete()
+    # @dp.message(F.text.lower().endswith("контакт"))
+    # async def contact(message: Message):
+    #     print(f"[LOG] функция contact запущена")
+    #     await message.answer_contact(phone_number="+123123",first_name="adsad")
+    #     print(f"[LOG] функция contact завершена")
+    # @dp.message(F.text.endswith("адрес"))
+    # async def geolocation(message: Message):
+    #     print(f"[LOG] функция geolocation запущена")
+    #     await message.answer_location(latitude=14,longitude=88)
+    #     print(f"[LOG] функция geolocation завершена")
+    # @dp.message(Command(commands="start"))
+    # async def start_handler(message: Message):
+    #     print(f"[LOG] функция start_handler запущена")
+    #     await message.answer("Привет")
+    #     print(f"[LOG] функция start_handler завершена")
+    # @dp.message()
+    # async def echo(message: Message):
+    #     await message.answer(message.text)
+    @dp.message(Command(commands=["silca"]))
+    async def silca(message: Message):
+        with open("data1.txt", "r") as f:
+            file = f.readlines()
+            s = await message.answer(f"Загрузка...")
+            for i in file[1:]:
+                num,link = i.split()
+                await s.edit_text(f"для ссылки {link} были загружены данные")
+                await asyncio.sleep(5)
     await dp.start_polling(bot)
 print(f'[LOG] Бот запущен')
 if __name__ == '__main__':
